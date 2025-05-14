84a2344058bfd04827410ceaa733ba60f3829f41
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-05-14 12:37:26 +0500

    auth js installed

--- src/auth.ts
+++ src/auth.ts
@@ -3,3 +3,5 @@ import NextAuth from "next-auth";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [],
 });
+// https://www.youtube.com/watch?v=OmX8fryikjg&ab_channel=codewithlari
+// 12:10
