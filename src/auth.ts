1cfdd1384508bb3e19c99cb2e5068d550f0a6b73
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-05-15 09:17:38 +0500

    10 issues solved

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 });
 // https://www.youtube.com/watch?v=OmX8fryikjg&ab_channel=codewithlari
-// 12:10
+// 18:15
